d6de4a04e62ca89c1b112c60e52c5e6e42239b03
matyasharvanek <matyas.harvanek@outlook.com>
2025-02-08 15:12:41 +0100

    Update

--- mine.lua
+++ mine.lua
@@ -1,6 +1,10 @@
 sizeX = 15
 sizeY = 15
 
+blocksDugDown = 0
+
+turtle.inspect()
+
 for x = 1, sizeX do
     for y = 1, sizeY do
         if turtle.detect() then
@@ -11,17 +15,33 @@ for x = 1, sizeX do
     if x % 2 == 0 then
         turtle.turnRight()
         
-        if turtle.detect() then
-            turtle.dig()
+        data = turtle.inspectDown()
+        while turtle.inspectDown().name == "minecraft:bedrock" then
+            blocksDugDown++
+            turtle.digDown()
+            turtle.down()
         end
+
+        for g = 1, blocksDugDown do
+            turtle.up()    
+        end
+        blocksDugDown = 0
         turtle.forward()
         turtle.turnRight()
     else
         turtle.turnLeft()
         
-        if turtle.detect() then
-            turtle.dig()
+        data = turtle.inspectDown()
+        while turtle.inspectDown().name == "minecraft:bedrock" then
+            blocksDugDown++
+            turtle.digDown()
+            turtle.down()
         end
+
+        for g = 1, blocksDugDown do
+            turtle.up()    
+        end
+        blocksDugDown = 0
         turtle.forward()
         turtle.turnLeft()
     end
