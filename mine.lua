fa2904c64bcf98d11c1a1137c767a7bed34388cf
matyasharvanek <matyas.harvanek@outlook.com>
2025-02-08 15:25:53 +0100

    fdf

--- mine.lua
+++ mine.lua
@@ -6,42 +6,40 @@ blocksDugDown = 0
 
 for x = 1, sizeX do
     for y = 1, sizeY do
-        if turtle.detect() then
-            turtle.dig()
+        if x % 2 == 0 then
+            turtle.turnRight()
+            
+            data = turtle.inspectDown()
+            while turtle.inspectDown().name == "minecraft:bedrock" do
+                blocksDugDown = blocksDugDown + 1
+                turtle.digDown()
+                turtle.down()
+            end
+    
+            for g = 1, blocksDugDown do
+                turtle.up()    
+            end
+            blocksDugDown = 0
+            turtle.forward()
+            turtle.turnRight()
+        else
+            turtle.turnLeft()
+            
+            data = turtle.inspectDown()
+            while turtle.inspectDown().name == "minecraft:bedrock" do
+                blocksDugDown = blocksDugDown + 1
+                turtle.digDown()
+                turtle.down()
+            end
+    
+            for g = 1, blocksDugDown do
+                turtle.up()    
+            end
+            blocksDugDown = 0
+            turtle.forward()
+            turtle.turnLeft()
         end
         turtle.forward()
     end
-    if x % 2 == 0 then
-        turtle.turnRight()
-        
-        data = turtle.inspectDown()
-        while turtle.inspectDown().name == "minecraft:bedrock" do
-            blocksDugDown = blocksDugDown + 1
-            turtle.digDown()
-            turtle.down()
-        end
-
-        for g = 1, blocksDugDown do
-            turtle.up()    
-        end
-        blocksDugDown = 0
-        turtle.forward()
-        turtle.turnRight()
-    else
-        turtle.turnLeft()
-        
-        data = turtle.inspectDown()
-        while turtle.inspectDown().name == "minecraft:bedrock" do
-            blocksDugDown = blocksDugDown + 1
-            turtle.digDown()
-            turtle.down()
-        end
-
-        for g = 1, blocksDugDown do
-            turtle.up()    
-        end
-        blocksDugDown = 0
-        turtle.forward()
-        turtle.turnLeft()
-    end
+    
 end
