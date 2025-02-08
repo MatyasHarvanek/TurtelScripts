7daf1e56cb9e78afaf63dff63313713a3ca4c501
matyasharvanek <matyas.harvanek@outlook.com>
2025-02-08 15:29:41 +0100

    f

--- mine.lua
+++ mine.lua
@@ -6,40 +6,39 @@ blocksDugDown = 0
 
 for x = 1, sizeX do
     for y = 1, sizeY do
-        if x % 2 == 0 then
-            turtle.turnRight()
-            
-            data = turtle.inspectDown()
-            while turtle.inspectDown().name == "minecraft:bedrock" do
-                blocksDugDown = blocksDugDown + 1
-                turtle.digDown()
-                turtle.down()
-            end
-    
-            for g = 1, blocksDugDown do
-                turtle.up()    
-            end
-            blocksDugDown = 0
-            turtle.forward()
-            turtle.turnRight()
-        else
-            turtle.turnLeft()
-            
-            data = turtle.inspectDown()
-            while turtle.inspectDown().name == "minecraft:bedrock" do
-                blocksDugDown = blocksDugDown + 1
-                turtle.digDown()
-                turtle.down()
-            end
-    
-            for g = 1, blocksDugDown do
-                turtle.up()    
-            end
-            blocksDugDown = 0
-            turtle.forward()
-            turtle.turnLeft()
+        if turtle.detect() then
+            turtle.dig()
         end
         turtle.forward()
+
+        data = turtle.inspectDown()
+        while turtle.inspectDown().name == "minecraft:bedrock" do
+            blocksDugDown = blocksDugDown + 1
+            turtle.digDown()
+            turtle.down()
+        end
+
+        for g = 1, blocksDugDown do
+            turtle.up()    
+        end
+        blocksDugDown = 0
     end
-    
+    if x % 2 == 0 then
+        turtle.turnRight()
+        
+        if turtle.detect() then
+            turtle.dig()
+        end
+        turtle.forward()
+        turtle.turnRight()
+    else
+        turtle.turnLeft()
+        
+        if turtle.detect() then
+            turtle.dig()
+        end
+        turtle.forward()
+        turtle.turnLeft()
+    end
 end
+
